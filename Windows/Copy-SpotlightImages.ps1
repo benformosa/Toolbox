5746729e4781936c406656d86b699701b6b505e1
Ben Formosa <ben.formosa@gmail.com>
2016-11-12 16:30:48 +1100

    CmdletBinding, parameters

--- Windows/Copy-SpotlightImages.ps1
+++ Windows/Copy-SpotlightImages.ps1
@@ -1,27 +1,92 @@
-﻿$SpotlightPath = Join-Path $env:LocalAppData "Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
-$DestinationPath = Join-Path $env:USERPROFILE "Pictures\Spotlight"
+﻿<#
+.SYNOPSIS 
+Copies images from the Windows Spotlight hidden folder.
 
-$Landscape = "Landscape"
-$Portrait = "Portrait"
+.DESCRIPTION
+Use this script to save the Spotlight lock screen images to use as desktop wallpaper.
+By default, images are saved to a folder "Spotlight" in your Pictures library. Portrait and Landscape images are sorted into subfolders.
 
-New-Item -ItemType Directory -Path $DestinationPath -Force | Out-Null
-New-Item -ItemType Directory -Path (Join-Path $DestinationPath $Landscape) -Force | Out-Null
-New-Item -ItemType Directory -Path (Join-Path $DestinationPath $Portrait) -Force | Out-Null
+.PARAMETER Destination
+Path to copy images to.
 
-foreach($file in (gci $SpotlightPath)) {
-    try {
-        $Image = New-Object System.Drawing.Bitmap $File.FullName
-        if ($Image.width -gt 500 -and $Image.height -gt 500) {
-            if($Image.width -gt $Image.height) {
-                $Subfolder = $Landscape
-            } else {
-                $Subfolder = $Portrait
-            }
-            $DestinationFile = (Join-Path "$($DestinationPath)\$($Subfolder)" "$($File.name).jpg")
-            Copy-Item $File.FullName -Destination $DestinationFile
-            Write-Verbose "Saved file $DestinationFile"
+.PARAMETER NoSubFolders
+Copy all images to the festination, rather than to a "Landscape" or "Portrait" subfolder.
+
+.PARAMETER LandscapeOnly
+Only copy images which are wider than they are tall.
+
+.PARAMETER PortraitOnly
+Only copy images which are taller than they are wide.
+
+.PARAMTER Force
+Always copy images, even if they already exist in the destination.
+#>
+
+[CmdletBinding(SupportsShouldProcess)]
+Param(
+[string] $Destination = (Join-Path $env:USERPROFILE "Pictures\Spotlight"),
+[switch] $NoSubFolders,
+[Parameter(ParameterSetName='LandscapeOnly')] [switch] $LandscapeOnly,
+[Parameter(ParameterSetName='PortraitOnly')] [switch] $PortraitOnly,
+[switch] $Force
+)
+
+begin {
+    $SpotlightPath = Join-Path $env:LocalAppData "Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+    $MinimumImageSize = 500
+    $Landscape = "Landscape"
+    $Portrait = "Portrait"
+
+    New-Item -ItemType Directory -Path $Destination -Force | Out-Null
+    Write-Verbose "Created directory $($Destination)"
+    if(-not $NoSubFolders) {
+        if(-not $PortraitOnly) {
+            New-Item -ItemType Directory -Path (Join-Path $Destination $Landscape) -Force | Out-Null
+            Write-Verbose "Created directory $(Join-Path $Destination $Landscape)"
         }
-    } catch {
-        Write-Verbose "Invalid file $File.FullName"
+        if(-not $LandscapeOnly) {
+            New-Item -ItemType Directory -Path (Join-Path $Destination $Portrait) -Force | Out-Null
+            Write-Verbose "Created directory $(Join-Path $Destination $Portrait)"
+        }
+    }
+}
+
+process {
+    foreach($File in (Get-ChildItem $SpotlightPath)) {
+        try {
+            # This will cause an exception for a file that isn't an image
+            $Image = New-Object System.Drawing.Bitmap $File.FullName
+
+            # Filter out images which are too small to be wallpapers
+            if ($Image.width -gt $MinimumImageSize -and $Image.height -gt $MinimumImageSize) {
+
+                if($Image.width -gt $Image.height) {
+                    $Aspect = $Landscape
+                } else {
+                    $Aspect = $Portrait
+                }
+
+                if($NoSubFolders) {
+                    $DestinationDir = $Destination
+                } else {
+                    $DestinationDir = Join-Path $Destination $Aspect
+                }
+
+                # Rename the file with a valid file extension
+                $DestinationFile = Join-Path $DestinationDir "$($File.name).jpg"
+
+                $Exists = -not $Force -and (Test-Path $DestinationFile)
+
+                if(-not $Exists -and
+                    ($Aspect -eq $Portrait -and -not $LandscapeOnly) -or
+                    ($Aspect -eq $Landscape -and -not $PortraitOnly)
+                    ) {
+                    Copy-Item $File.FullName -Destination $DestinationFile
+                    Write-Verbose "Copied $($Aspect) image: $($DestinationFile)"
+                }
+            }
+        } catch {
+            Write-Verbose "Not an image file: $($File.Name)"
+        }
     }
 }

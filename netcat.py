d4a63dd23c1c7af7937e2015f150b71e3df8e0db
Ben Formosa <ben.formosa@gmail.com>
2017-05-18 21:29:14 +1000

    Cleanup setting source address

--- netcat.py
+++ netcat.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
+"""
+An implementation of netcat compatible with Python 2 and 3.
+Written because netcat wasn't installed on Oracle Linux 7.3
+"""
 import argparse
 import socket
 import sys
 
 parser = argparse.ArgumentParser(description='Simple netcat in pure python.')
-parser.add_argument('-s', '--source', metavar='ADDRESS')
+parser.add_argument('-s', '--source', metavar='ADDRESS', default='')
 parser.add_argument('-v', '--verbose', action='store_true')
 parser.add_argument('-w', '--wait', metavar='SECONDS', type=int)
 parser.add_argument('-z', '--zero', action='store_true')
@@ -12,16 +16,10 @@ parser.add_argument('host')
 parser.add_argument('port')
 args = parser.parse_args()
 
-
-# Set a souce address for socket connection
-source = ('', 0)
-if args.source:
-    source = (args.source, 0) # port 0 specifies that the OS will choose a port
-
 # exit successfully if the connection succeeds
 if args.zero:
     try:
-        connection = socket.create_connection((args.host, args.port), args.wait, source)
+        connection = socket.create_connection((args.host, args.port), args.wait, (args.source, 0))
         if args.verbose:
             print("Connection to {} {} port (tcp) succeeded!".format(args.host, args.port))
         sys.exit(0)

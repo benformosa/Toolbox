d2154122e96abe79a136838f67423bd72d8d2dd6
Ben Formosa <ben.formosa@gmail.com>
2018-08-08 14:03:11 +1000

    yamlcol input check

--- yamlcol.py
+++ yamlcol.py
@@ -4,16 +4,20 @@ import argparse
 import yaml
 import sys
 
-parser = argparse.ArgumentParser(description='Extract values for a single key from a yaml sequence of mappings')
+parser = argparse.ArgumentParser(description='Extract values for a single key from a YAML sequence of mappings')
 parser.add_argument('key', help='Extract values for this key.')
 args = parser.parse_args()
 
 # Load YAML from standard in
 data = yaml.safe_load(sys.stdin)
 
-# List comprehension to extract value by key
-values = [d[args.key] for d in data if args.key in d]
+if type(data) is list:
+    # List comprehension to extract value by key
+    values = [d[args.key] for d in data if args.key in d]
 
-# Print values
-for v in values:
-    print(v)
+    # Print values
+    for v in values:
+        print(v)
+else:
+    print("Error: data is not a YAML sequence")
+    sys.exit(1)

027b1e35a1808b010b30c596b9e98ede72b8c4e4
Ben Formosa <ben.formosa@gmail.com>
2018-08-10 09:34:00 +1000

    add yamladd

---
+++ yamladd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+import argparse
+import yaml
+import sys
+
+parser = argparse.ArgumentParser(
+        description='Insert a key-value pair into each mapping '
+        'in a YAML sequence of mappings')
+parser.add_argument('key', help='Add this key')
+parser.add_argument('value', help='with this value')
+args = parser.parse_args()
+
+# Load YAML from standard in
+data = yaml.safe_load(sys.stdin)
+
+if type(data) is list:
+    for d in data:
+        d[args.key] = args.value
+    print(yaml.safe_dump(data, default_flow_style=False))
+
+else:
+    print("Error: data is not a YAML sequence")
+    sys.exit(1)
